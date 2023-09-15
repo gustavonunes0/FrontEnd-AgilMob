fae2545cf9123fc871478edb28f5d0746ed1c87b
Gustavo Nunes <108197715+gustavonunes0@users.noreply.github.com>
2023-09-15 11:10:08 -0300

    Update produtos.js

--- src/styles/produtos.js
+++ src/styles/produtos.js
@@ -21,6 +21,13 @@ export const Main = styled.main`
   }
 `;
 
+export const BotaoWpp = styled.a`
+  position: fixed;
+  bottom: 20px; 
+  right: 20px;
+  z-index: 1000;
+`;
+
 export const TipoProdutos = styled.a`
   width: 100%;
   height: auto;
